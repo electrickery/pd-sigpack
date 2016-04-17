625a8d1e55c8fae653012fa59908f77a7fd5a6bf
Fred Jan Kraan <fjkraan@xs4all.nl>
2016-04-17 09:59:36 +0200

    transient~: added useful test signal to the help patch for testing and optimized/updated the code in the process

--- source/transient~.c
+++ source/transient~.c
@@ -16,7 +16,7 @@
 #define ASTAB 0.02f
 //Truncate-to-zero modulo (ANSI C doesn`t specify) will only work
 //if -m < v < 2m
-#define MOD(v,m) (v<0?v+m:(v>=m?v-m:v))
+#define MOD(v,m) (v < 0 ? v+m : (v >= m ? v-m : v))
 
 // ------------------------ transient~ ----------------------------- 
 // transient mangler
@@ -29,17 +29,17 @@ typedef struct _transient_tilde
     t_object x_obj;
 	t_sample x_attack;//-1 - 1
 	t_sample x_sustain;//-1 - 1
-	float *x_buffer;
+	t_float *x_buffer;
 	int x_buffer_pos;
 	long x_count;
-	float x_fast_buffer_sum;
-	float x_fast_track;
-	float x_medi_buffer_sum;
-	float x_medi_track;
+	t_float x_fast_buffer_sum;
+	t_float x_fast_track;
+	t_float x_medi_buffer_sum;
+	t_float x_medi_track;
 	int x_sample_rate;
-	float x_slow_buffer_sum;
-	float x_slow_track;
-	float x_f;
+	t_float x_slow_buffer_sum;
+	t_float x_slow_track;
+	t_float x_f;
 } t_transient_tilde;
 
 static void *transient_tilde_new(t_floatarg attack, t_floatarg sustain)
@@ -72,50 +72,76 @@ static void *transient_tilde_new(t_floatarg attack, t_floatarg sustain)
 static t_int *transient_tilde_perform(t_int *w)
 {
 	t_transient_tilde *x = (t_transient_tilde *)(w[1]);
-    t_float *in = (t_float *)(w[2]);
-    t_float *out = (t_float *)(w[3]);
+    t_float *inVal = (t_float *)(w[2]);
+    t_float *outVal = (t_float *)(w[3]);
     int n = (int)(w[4]);
-	float f;
 	const int fast_sum_size = (2 * x->x_sample_rate) / 1000;
 	const int medi_sum_size = (25 * x->x_sample_rate) / 1000;
 	const int slow_sum_size = (100 * x->x_sample_rate) / 1000;
 	const float fast_track_lag = 1.5f / fast_sum_size;
 	const float medi_track_lag = 1.0f / medi_sum_size;
 	const float slow_track_lag = 1.3f / slow_sum_size;
-	float ratio;
+	t_float ratio;
+    
+	t_float attack          = x->x_attack;
+	t_float sustain         = x->x_sustain;
+    t_float *buffer         = x->x_buffer;
+ 	int buffer_pos          = x->x_buffer_pos;
+	long count              = x->x_count;
+	t_float fast_buffer_sum = x->x_fast_buffer_sum;
+	t_float fast_track      = x->x_fast_track;
+	t_float medi_buffer_sum = x->x_medi_buffer_sum;
+	t_float medi_track      = x->x_medi_track;
+	t_float slow_buffer_sum = x->x_slow_buffer_sum;
+	t_float slow_track      = x->x_slow_track;
+    t_float in;
 
     while (n--)
     {
-		f = *in++;
-		x->x_buffer[x->x_buffer_pos] = fabs(f);
-		x->x_fast_buffer_sum += x->x_buffer[x->x_buffer_pos];
-		x->x_medi_buffer_sum += x->x_buffer[x->x_buffer_pos];
-		x->x_slow_buffer_sum += x->x_buffer[x->x_buffer_pos];
-		x->x_fast_buffer_sum -= x->x_buffer[MOD(x->x_buffer_pos - fast_sum_size, BUFFER_SIZE)];
-		x->x_medi_buffer_sum -= x->x_buffer[MOD(x->x_buffer_pos - medi_sum_size, BUFFER_SIZE)];
-		x->x_slow_buffer_sum -= x->x_buffer[MOD(x->x_buffer_pos - slow_sum_size, BUFFER_SIZE)];
-		if(x->x_count++ > slow_sum_size) {
-			x->x_fast_track += (x->x_fast_buffer_sum/fast_sum_size - x->x_fast_track) * fast_track_lag;
-			x->x_medi_track += (x->x_medi_buffer_sum/medi_sum_size - x->x_medi_track) * medi_track_lag;
-			x->x_slow_track += (x->x_slow_buffer_sum/slow_sum_size - x->x_slow_track) * slow_track_lag;
-		}
-		//Attack
-		ratio = (x->x_fast_track + ASTAB) / (x->x_medi_track + ASTAB);
-		if (ratio * x->x_attack > 1.0f) {
-			f *= ratio * x->x_attack;
-		} else if (ratio * x->x_attack < 1.0f) {
-			f /= ratio * -x->x_attack;
-		}
-		//Sustain
-		ratio = (x->x_slow_track + SSTAB) / (x->x_medi_track + SSTAB);
-		if (ratio * x->x_sustain > 1.0f) {
-			f *= ratio * x->x_sustain;
-		} else if (ratio * x->x_sustain < -1.0f) {
-			f /= ratio * x->x_sustain;
-		}
-		*out++ = f;
-		x->x_buffer_pos = (x->x_buffer_pos +1) % BUFFER_SIZE;
+		in = *inVal++;
+        buffer[buffer_pos] = fabs(in);
+        fast_buffer_sum += buffer[buffer_pos];
+        medi_buffer_sum += buffer[buffer_pos];
+        slow_buffer_sum += buffer[buffer_pos];
+        fast_buffer_sum -= buffer[MOD(buffer_pos - fast_sum_size, BUFFER_SIZE)];
+        medi_buffer_sum -= buffer[MOD(buffer_pos - medi_sum_size, BUFFER_SIZE)];
+        slow_buffer_sum -= buffer[MOD(buffer_pos - slow_sum_size, BUFFER_SIZE)];
+        if (count++ > slow_sum_size) {
+                fast_track += (fast_buffer_sum/fast_sum_size - fast_track)
+                 * fast_track_lag;
+                medi_track += (medi_buffer_sum/medi_sum_size - medi_track)
+                 * medi_track_lag;
+                slow_track += (slow_buffer_sum/slow_sum_size - slow_track)
+                 * slow_track_lag;
+        }
+
+        // Attack
+        ratio = (fast_track + ASTAB) / (medi_track + ASTAB);
+        if (ratio * attack > 1.0f) {
+                in *= ratio * attack;
+        } else if (ratio * attack < -1.0f) {
+                in /= ratio * -attack;
+        }
+
+        // Sustain
+        ratio = (slow_track + SSTAB) / (medi_track + SSTAB);
+        if (ratio * sustain > 1.0f) {
+                in *= ratio * sustain;
+        } else if (ratio * sustain < -1.0f) {
+                in /= ratio * -sustain;
+        }
+		*outVal++ = in;
+		buffer_pos = (buffer_pos + 1) % BUFFER_SIZE;
     }
+    x->x_buffer_pos      = buffer_pos;
+    x->x_count           = count;
+    x->x_fast_buffer_sum = fast_buffer_sum;
+    x->x_fast_track      = fast_track;
+    x->x_medi_buffer_sum = medi_buffer_sum;
+    x->x_medi_track      = medi_track;
+    x->x_slow_buffer_sum = slow_buffer_sum;
+    x->x_slow_track      = slow_track;
+    
     return (w+5);
 }
 
